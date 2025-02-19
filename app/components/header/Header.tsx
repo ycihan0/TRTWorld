7e7f74fedcda5cb8264e84332bffd04b1c4ff35b
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2025-02-19 13:26:51 +0300

    google font CSS import

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,55 @@
+
 import styles from "./Header.module.css";
+//import { useState } from "react";
 
 const Header: React.FC = () => {
-  return <div className={styles.container}></div>;
-};
+  // const [openDropdown, setOpenDropdown] = useState(null);
 
+  // const toggleDropdown = (menu) => {
+  //   setOpenDropdown(openDropdown === menu ? null : menu);
+  // };
+
+  return (
+    <header className={styles.header}>
+    <div className={styles.container}>
+      <div className={styles.logo}>TRT <span>WORLD</span></div>
+      <nav className={styles.nav}>
+        <ul className={styles.menu}>
+          <li 
+            className={styles.menuItem} 
+            // onMouseEnter={() => toggleDropdown('news')}
+            // onMouseLeave={() => toggleDropdown(null)}
+          >
+            News
+            {/* {openDropdown === 'news' && (
+              <ul className={styles.dropdown}>
+                <li>Latest</li>
+                <li>World</li>
+                <li>Politics</li>
+              </ul>
+            )} */}
+          </li>
+          <li 
+            className={styles.menuItem}
+            // onMouseEnter={() => toggleDropdown('features')}
+            // onMouseLeave={() => toggleDropdown(null)}
+          >
+            Features
+            {/* {openDropdown === 'features' && (
+              <ul className={styles.dropdown}>
+                <li>Analysis</li>
+                <li>Opinions</li>
+                <li>Investigations</li>
+              </ul>
+            )} */}
+          </li>
+          <li className={styles.menuItem}>Topics</li>
+          <li className={styles.menuItem}>Video</li>
+          <li className={styles.menuItemLive}>LIVE</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  )
+}
 export default Header;
