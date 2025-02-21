99240f0a75f8f9e259dce427dba071c10a41a880
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2025-02-21 17:10:46 +0300

    responsive bugs

--- app/components/singleNews/SingleNews.tsx
+++ app/components/singleNews/SingleNews.tsx
@@ -22,29 +22,26 @@ const SingleNews: React.FC<{ news: NewsItem }> = ({ news }) => {
           className={styles.image}
         />
       </div>
-      <div className={styles.content}>
-        <span className={styles.category}>
-          {news.category} <span className={styles.date}>{news.date}</span>
-        </span>
-        <h2 className={styles.title}>{news.title}</h2>
-        <p className={styles.description}>{news.description}</p>
-        <div className={styles.sidebarItem}>
-          <div className={styles.avatar}>
-            <Image src="/images/biden.jpg" alt="" width={48} height={48} />
-          </div>
-          <div className={styles.newsContent}>
-            <span className={styles.author}>Valentino Chooses</span>
-          </div>
-          
+
+      <span className={styles.category}>
+        {news.category} <span className={styles.date}>{news.date}</span>
+      </span>
+      <h2 className={styles.title}>{news.title}</h2>
+      <p className={styles.description}>{news.description}</p>
+      <div className={styles.sidebarItem}>
+        <div className={styles.avatar}>
+          <Image src="/images/biden.jpg" alt="" width={48} height={48} />
         </div>
-        <div className={styles.sidebarItem}>
-          <div className={styles.avatar}>
-            <Image src="/images/biden.jpg" alt="" width={48} height={48} />
-          </div>
-          <div className={styles.newsContent}>
-            <span className={styles.author}>Valentino Chooses</span>
-          </div>
-          
+        <div className={styles.newsContent}>
+          <span className={styles.author}>Valentino Chooses</span>
+        </div>
+      </div>
+      <div className={styles.sidebarItem}>
+        <div className={styles.avatar}>
+          <Image src="/images/biden.jpg" alt="" width={48} height={48} />
+        </div>
+        <div className={styles.newsContent}>
+          <span className={styles.author}>Valentino Chooses</span>
         </div>
       </div>
     </div>
