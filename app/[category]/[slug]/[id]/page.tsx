51f8f9c8d57205faa7b5662614a3138952763dc3
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2025-02-22 21:52:29 +0300

    bug fixed about params

--- app/[category]/[slug]/[id]/page.tsx
+++ app/[category]/[slug]/[id]/page.tsx
@@ -1,44 +1,37 @@
 import { News } from "@/types/news";
 import { notFound } from "next/navigation";
 
-async function getNews(): Promise<News[]> {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/news`, {
-      cache: "no-store",
-    });
-    if (!res.ok) throw new Error("Failed to fetch news");
-    return await res.json();
-  } catch (error) {
-    console.error("Error fetching news:", error);
-    return [];
-  }
-}
 
-export default async function NewsDetailPage({
-  params,
-}: {
+
+export default async function NewsDetailPage(props: {
   params: { category: string; slug: string; id: string };
-}) {
-  const newsList = await getNews();
+}){
 
-  // Haberleri kategori, slug ve id'ye göre filtrele
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/news`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    console.error("Error fetching news");
+    notFound();
+  }
+  const newsList: News[] = await res.json();
+
+  const params = await props.params; 
+  const category = params.category;
+  const slug = params.slug;
+  const id = params.id;
+
   const newsItem = newsList.find(
-    (news) =>
-      news.category === params.category &&
-      news.slug === params.slug &&
-      news.id === params.id
+    (news) => news.category === category && news.slug === slug && news.id === id
   );
-
-  // Eğer haber bulunamazsa 404 sayfasına yönlendir
-  if (!newsItem) {
+ if (!newsItem) {
     notFound();
   }
 
   return (
     <div>
-      <h1>{newsItem.title}</h1>
-      <p>{newsItem.subtitle}</p>
-      <div dangerouslySetInnerHTML={{ __html: newsItem.content }} />
+     {newsItem.subtitle}
     </div>
   );
 }
