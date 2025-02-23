15375811c14642991ee1852084c47578f95a30f8
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2025-02-23 15:03:42 +0300

    dynamic search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import styles from "./SearchPage.module.css";
+import { News } from "@/types/news";
+import NewsList from "../components/newsList/NewsList";
 
-const page: React.FC = () => {
+const Page: React.FC = () => {
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<News[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (query.length > 2) {
+        try {
+          const response = await fetch(
+            `https://trtworldapi.onrender.com/news?q=${query}`
+          );
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data: News[] = await response.json();
+          setResults(data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      } else {
+        setResults([]);
+      }
+    };
+    fetchData();
+  }, [query]);
+
   return (
-    <div>
+    <>
       <div className={styles.searchBarBackground}>
         <div className={styles.container}>
           <div className={styles.searchContainer}>
@@ -15,13 +44,20 @@ const page: React.FC = () => {
                 type="text"
                 className={styles.searchBar}
                 placeholder="News, Podcasts and more..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
               />
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.result}>
+          {results.length > 0 ? <NewsList lastestNews={results} /> : ""}
+        </div>
+      </div>
+    </>
   );
 };
 
-export default page;
+export default Page;
